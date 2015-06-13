cf3ef4e900de55f813d9dd0af2e382165f6efdfc
Index Int <vlad.z.4096@gmail.com>
2015-06-13 18:42:12 +0300

    Wrapped MonadWriter

--- src/Control/Ether/Wrapped.hs
+++ src/Control/Ether/Wrapped.hs
@@ -11,13 +11,15 @@ import Control.Applicative (Alternative)
 import Control.Monad (MonadPlus)
 import Control.Monad.Fix (MonadFix)
 import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Ether.Reader
-import Control.Monad.Ether.State
-import Control.Monad.Ether.Except
+import Control.Monad.Ether.Reader.Class
+import Control.Monad.Ether.State.Class
+import Control.Monad.Ether.Except.Class
+import Control.Monad.Ether.Writer.Class
 
 import qualified Control.Monad.Reader as Class
 import qualified Control.Monad.State  as Class
 import qualified Control.Monad.Except as Class
+import qualified Control.Monad.Writer as Class
 
 newtype WrappedEther tag m a = WrapEther { unwrapEther :: m a }
     deriving ( Functor, Applicative, Alternative, Monad, MonadPlus
@@ -34,6 +36,11 @@ instance MonadState tag s m => Class.MonadState s (WrappedEther tag m) where
 instance MonadExcept tag e m => Class.MonadError e (WrappedEther tag m) where
     throwError = WrapEther . throw (Proxy :: Proxy tag)
     catchError m h = WrapEther $ catch (Proxy :: Proxy tag) (unwrapEther m) (unwrapEther . h)
+
+instance MonadWriter tag w m => Class.MonadWriter w (WrappedEther tag m) where
+    tell = WrapEther . tell (Proxy :: Proxy tag)
+    listen = WrapEther . listen (Proxy :: Proxy tag) . unwrapEther
+    pass = WrapEther . pass (Proxy :: Proxy tag) . unwrapEther
 
 ethered :: proxy tag -> WrappedEther tag m a -> m a
 ethered _proxy = unwrapEther
