49e3d4f4fdc75e79b0a62388796267081b8c1e69
Index Int <vlad.z.4096@gmail.com>
2015-11-19 12:09:52 +0300

    MonadBase and MonadBaseControl for WrapEther

--- src/Control/Ether/Wrapped.hs
+++ src/Control/Ether/Wrapped.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 {- |
@@ -33,6 +34,7 @@ import Control.Monad (MonadPlus)
 import Control.Monad.Fix (MonadFix)
 import Control.Monad.IO.Class (MonadIO)
 import GHC.Generics (Generic)
+import Data.Coerce (coerce)
 
 import Control.Monad.Ether.Reader.Class
 import Control.Monad.Ether.State.Class
@@ -44,6 +46,9 @@ import qualified Control.Monad.State  as Class
 import qualified Control.Monad.Except as Class
 import qualified Control.Monad.Writer as Class
 
+import qualified Control.Monad.Base as MB
+import qualified Control.Monad.Trans.Control as MC
+
 -- | Wrap a monad to attach a tag to it.
 newtype WrappedEther tag m a = WrapEther { unwrapEther :: m a }
     deriving ( Generic
@@ -52,15 +57,23 @@ newtype WrappedEther tag m a = WrapEther { unwrapEther :: m a }
 
 -- | Annotate a polymorphic monadic computation with a tag.
 ethered :: proxy tag -> WrappedEther tag m a -> m a
-ethered _proxy = unwrapEther
+ethered _proxy = coerce
+
+instance MB.MonadBase b m => MB.MonadBase b (WrappedEther tag m) where
+  liftBase a = WrapEther (MB.liftBase a)
+
+instance MC.MonadBaseControl b m => MC.MonadBaseControl b (WrappedEther tag m) where
+  type StM (WrappedEther tag m) a = MC.StM m a
+  liftBaseWith a = WrapEther (MC.liftBaseWith (a . coerce))
+  restoreM a = WrapEther (MC.restoreM a)
 
 instance MonadReader tag r m => Class.MonadReader r (WrappedEther tag m) where
     ask = WrapEther $ ask (Proxy :: Proxy tag)
-    local f = WrapEther . local (Proxy :: Proxy tag) f . unwrapEther
+    local f = WrapEther . local (Proxy :: Proxy tag) f . coerce
 
 instance MonadReader tag r m => MonadReader tag r (WrappedEther tag' m) where
     ask t = WrapEther $ ask t
-    local t f = WrapEther . local t f . unwrapEther
+    local t f = WrapEther . local t f . coerce
 
 instance MonadState tag s m => Class.MonadState s (WrappedEther tag m) where
     get = WrapEther $ get (Proxy :: Proxy tag)
@@ -72,18 +85,18 @@ instance MonadState tag s m => MonadState tag s (WrappedEther tag' m) where
 
 instance MonadExcept tag e m => Class.MonadError e (WrappedEther tag m) where
     throwError = WrapEther . throw (Proxy :: Proxy tag)
-    catchError m h = WrapEther $ catch (Proxy :: Proxy tag) (unwrapEther m) (unwrapEther . h)
+    catchError m h = WrapEther $ catch (Proxy :: Proxy tag) (coerce m) (coerce . h)
 
 instance MonadExcept tag e m => MonadExcept tag e (WrappedEther tag' m) where
     throw t = WrapEther . throw t
-    catch t m h = WrapEther $ catch t (unwrapEther m) (unwrapEther . h)
+    catch t m h = WrapEther $ catch t (coerce m) (coerce . h)
 
 instance MonadWriter tag w m => Class.MonadWriter w (WrappedEther tag m) where
     tell = WrapEther . tell (Proxy :: Proxy tag)
-    listen = WrapEther . listen (Proxy :: Proxy tag) . unwrapEther
-    pass = WrapEther . pass (Proxy :: Proxy tag) . unwrapEther
+    listen = WrapEther . listen (Proxy :: Proxy tag) . coerce
+    pass = WrapEther . pass (Proxy :: Proxy tag) . coerce
 
 instance MonadWriter tag w m => MonadWriter tag w (WrappedEther tag' m) where
     tell t = WrapEther . tell t
-    listen t = WrapEther . listen t . unwrapEther
-    pass t = WrapEther . pass t . unwrapEther
+    listen t = WrapEther . listen t . coerce
+    pass t = WrapEther . pass t . coerce
