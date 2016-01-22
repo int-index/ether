4f574d744cb3528f5f313797a8c8932e0505388a
Index Int <vlad.z.4096@gmail.com>
2016-01-23 00:17:46 +0300

    Test-suite update (T5)

--- test/Regression.hs
+++ test/Regression.hs
@@ -40,32 +40,6 @@ suite = testGroup "Ether"
   , test6
   ]
 
-stateCore :: Ether '[S1 <-> Int, R1 --> Int] m => m ()
-stateCore = do
-    a <- ask r1
-    n <- get s1
-    put s1 (n * a)
-    modify s1 (subtract 1)
-
-recurseCore :: (Num a, Ord a) => Ether '[I.R a, I.S Int]m => m a
-recurseCore = do
-    a <- I.ask
-    if (a <= 0)
-        then do
-            I.put (0 :: Int)
-            return 1
-        else do
-            I.modify (succ :: Int -> Int)
-            b <- I.runReaderT recurseCore (a - 1)
-            I.modify (succ :: Int -> Int)
-            return (a * b)
-
-factorial :: (Num a, Ord a) => a -> (a, Int)
-factorial a = I.runState (I.runReaderT recurseCore a) (0 :: Int)
-
-factorial' :: Int -> (Int, Int)
-factorial' a = factorial (a :: Int)
-
 data DivideByZero = DivideByZero
     deriving (Show)
 data NegativeLog a = NegativeLog a
--- test/Regression/T5.hs
+++ test/Regression/T5.hs
@@ -1,6 +1,44 @@
 module Regression.T5 (test5) where
 
+import Control.Ether.Abbr
+
+import qualified Control.Monad.Ether.Implicit as I
+import qualified Control.Ether.Implicit.Abbr as I
+
 import Test.Tasty
+import Test.Tasty.QuickCheck
+
+newtype Counter = Counter Int
+  deriving (Eq, Ord, Num, Enum)
+
+incCounter :: Counter -> Counter
+incCounter = succ
+
+testEther
+  :: (Num a, Ord a) => Ether '[I.R a, I.S Counter] m
+  => m a
+testEther = do
+  a <- I.ask
+  if a <= 0
+    then do
+      I.put (0 :: Counter)
+      return 1
+    else do
+      I.modify incCounter -- overriden in the base case
+      b <- I.runReaderT testEther (a - 1)
+      I.modify incCounter
+      return (a * b)
+
+runner :: (Num a, Ord a) => a -> (a, Counter)
+runner a = I.runState (I.runReaderT testEther a) (0 :: Counter)
+
+factorial :: (Num a, Enum a) => a -> a
+factorial a = product [1..a]
 
 test5 :: TestTree
-test5 = testGroup "T5:" []
+test5 = testGroup "T5: Factorial via Ether"
+  [ testProperty "runner works"
+    $ \n -> property
+    $ let n' = fromIntegral n :: Integer
+      in runner n' == (factorial n', max 0 (Counter n))
+  ]
