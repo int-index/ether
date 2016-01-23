c9a138ca22b9e55874568a8a3928a26a57b0e5d3
Index Int <vlad.z.4096@gmail.com>
2016-01-23 17:41:13 +0300

    Test-suite update (T8)

--- test/Regression.hs
+++ test/Regression.hs
@@ -1,13 +1,5 @@
 module Main where
 
-import Control.Monad
-
-import Control.Monad.Ether
-import Control.Ether.Abbr
-
-import qualified Control.Monad.Reader as T
-import qualified Control.Monad.State  as T
-
 import Test.Tasty
 
 import Regression.T1
@@ -18,9 +10,6 @@ import Regression.T5
 import Regression.T6
 import Regression.T7
 import Regression.T8
-
-ethereal "Foo" "foo"
-ethereal "Bar" "bar"
 
 main :: IO ()
 main = defaultMain suite
@@ -36,40 +25,3 @@ suite = testGroup "Ether"
   , test7
   , test8
   ]
-
-wrapState_f :: T.MonadState Int m => m String
-wrapState_f = liftM show T.get
-
-wrapState_g :: T.MonadState Bool m => m String
-wrapState_g = liftM show T.get
-
-wrapState_useboth :: Ether '[Foo <-> Int, Bar <-> Bool] m => m String
-wrapState_useboth = do
-    a <- tagAttach foo wrapState_f
-    b <- tagAttach bar wrapState_g
-    return (a ++ b)
-
-wrapStateCore :: Int -> Bool -> String
-wrapStateCore int bool = evalState foo (evalStateT bar wrapState_useboth bool) int
-
-wrapStateBad1_g :: MonadState Foo Int m => m ()
-wrapStateBad1_g = modify foo (*100)
-
-wrapStateBad1_useboth :: MonadState Foo Int m => m String
-wrapStateBad1_useboth = do
-    wrapStateBad1_g
-    tagAttach foo wrapState_f
-
-wrapStateBad1 :: Int -> String
-wrapStateBad1 = evalState foo wrapStateBad1_useboth
-
-wrapStateBad2 :: (T.MonadState Int m , MonadState Foo Int m) => m Int
-wrapStateBad2 = do
-    modify foo (*100)
-    T.get
-
-wrapStateBad2Core :: Int -> Int
-wrapStateBad2Core = evalState foo (tagAttach foo wrapStateBad2)
-
-wrapReaderCore :: Int -> Int
-wrapReaderCore = runReader foo (tagAttach foo (liftM2 (+) T.ask (ask foo)))
--- test/Regression/T8.hs
+++ test/Regression/T8.hs
@@ -1,6 +1,52 @@
 module Regression.T8 (test8) where
 
+import Control.Ether.Abbr
+import Control.Monad.Ether
+
+import qualified Control.Monad.State as T
+
 import Test.Tasty
+import Test.Tasty.QuickCheck
+
+ethereal "Foo" "foo"
+ethereal "Bar" "bar"
+
+testMTL1 :: T.MonadState Int m => m ()
+testMTL1 = T.modify negate
+
+testMTL2 :: T.MonadState Bool m => m ()
+testMTL2 = T.modify not
+
+testEther
+  :: Ether '[Foo <-> Int, Bar <-> Bool] m
+  => m String
+testEther = do
+  tagAttach foo testMTL1
+  tagAttach bar testMTL2
+  a <- gets foo show
+  b <- gets bar show
+  return (a ++ b)
+
+model :: Int -> Bool -> String
+model a b = show (negate a) ++ show (not b)
+
+runner1 a b
+  = flip (evalState  foo) a
+  . flip (evalStateT bar) b
+
+runner2 a b
+  = flip (evalState  bar) b
+  . flip (evalStateT foo) a
 
 test8 :: TestTree
-test8 = testGroup "T8:" []
+test8 = testGroup "T8: Multiple tag attachements"
+  [ testProperty "runner₁ works"
+    $ \a b -> property
+    $ runner1 a b testEther == model a b
+  , testProperty "runner₂ works"
+    $ \a b -> property
+    $ runner2 a b testEther == model a b
+  , testProperty "runner₁ == runner₂"
+    $ \a b -> property
+    $ runner1 a b testEther == runner2 a b testEther
+  ]
