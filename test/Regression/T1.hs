024a8839bc61b6e114e13d3892d852d774d881f4
Vladislav Zavialov <vlad.z.4096@gmail.com>
2017-04-29 00:01:04 +0300

    Test flattened Reader in T1

--- test/Regression/T1.hs
+++ test/Regression/T1.hs
@@ -1,6 +1,7 @@
 module Regression.T1 (test1) where
 
 import Ether
+import Data.List (group)
 
 import Test.Tasty
 import Test.Tasty.QuickCheck
@@ -23,6 +24,10 @@ testEther = do
 
 runner1 s1 s2 = flip (runReader @Tag1) s1 . flip (runReaderT @Tag2) s2
 runner2 s1 s2 = flip (runReader @Tag2) s2 . flip (runReaderT @Tag1) s1
+runner3 s1 s2 = flip runReaders (Tagged @Tag1 s1, Tagged @Tag2 s2)
+
+same :: Eq a => [a] -> Bool
+same = (<=1) . length . group
 
 test1 :: TestTree
 test1 = testGroup "T1: Reader local environment"
@@ -32,7 +37,13 @@ test1 = testGroup "T1: Reader local environment"
   , testProperty "runner₂ works"
       $ \s1 s2 -> property
       $ runner2 s1 s2 testEther == ((s1, s2), (s1, map succ s2))
-  , testProperty "runner₁ == runner₂"
+  , testProperty "runner₃ works"
       $ \s1 s2 -> property
-      $ runner1 s1 s2 testEther == runner2 s1 s2 testEther
+      $ runner3 s1 s2 testEther == ((s1, s2), (s1, map succ s2))
+  , testProperty "runner₁ == runner₂ == runner₃"
+      $ \s1 s2 -> property
+      $ same
+        [ runner1 s1 s2 testEther
+        , runner2 s1 s2 testEther
+        , runner3 s1 s2 testEther ]
   ]
