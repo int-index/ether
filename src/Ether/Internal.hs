b40212aa60d9784f2c1dc4b650117ddeeecbe428
Vladislav Zavialov <vlad.z.4096@gmail.com>
2017-05-13 16:42:36 +0300

    Simplify TH code for tuple instances

--- src/Ether/Internal.hs
+++ src/Ether/Internal.hs
@@ -19,7 +19,6 @@ module Ether.Internal
   ) where
 
 import Control.Applicative
-import Control.Monad
 import Data.Coerce
 import Data.Functor.Identity
 import Data.Kind
@@ -72,6 +71,9 @@ instance HasLens a a a where
   lensOf = id
   {-# INLINE lensOf #-}
 
+instance HasLens t (Tagged t a) a where
+  lensOf = \f -> fmap coerce . f . coerce
+
 view :: LensLike (Const a) s t a b -> s -> a
 view l = coerce (l Const)
 {-# INLINE view #-}
@@ -93,87 +95,83 @@ return []
 
 type instance TagsK () = '[]
 type instance TagsK (Tagged t a) = '[KindOf t]
-type instance TagsK (Tagged t0 a, Tagged t1 b) = '[KindOf t0, KindOf t1]
+
+do
+  let
+    tupCount = 62
+    varName k = TH.mkName ('a' : show k)
+    tagName k = TH.mkName ('t' : show k)
+  for [2..tupCount] $ \n -> do
+    let
+      tupTy = List.foldl' TH.AppT (TH.ConT (TH.tupleTypeName n)) $
+        (\k -> TH.ConT ''Tagged `TH.AppT`
+          TH.VarT (tagName k) `TH.AppT`
+          TH.VarT (varName k)) <$> [0..n-1]
+      tagsList = List.foldr
+        (\a b -> TH.PromotedConsT `TH.AppT` a `TH.AppT` b)
+        TH.PromotedNilT
+        (TH.AppT (TH.ConT ''KindOf) . TH.VarT . tagName <$> [0..n-1])
+    return $
+      TH.TySynInstD ''TagsK (TH.TySynEqn [tupTy] tagsList)
 
 return []
 
 type instance Tags () = 'HNil
 type instance Tags (Tagged t a) = 'HCons t 'HNil
-type instance Tags (Tagged t0 a, Tagged t1 b) = 'HCons t0 ('HCons t1 'HNil)
 
 do
   let
     tupCount = 62
-    names    = [1..] >>= flip replicateM ['a'..'z']
-  varNames <- traverse TH.newName (take tupCount names)
-  fmap List.concat $
-    for (List.drop 2 (List.inits varNames)) $
-      \varNames' -> do
-        let n = List.length varNames'
-        tagsInstances <- for [() | n > 2] $ \() -> do
-          tag <- TH.newName "tag"
-          let
-            (cur:rest) = varNames'
-            tupTy = foldl TH.AppT (TH.ConT (TH.tupleTypeName n))
-              ( TH.ConT ''Tagged `TH.AppT` TH.VarT tag `TH.AppT` TH.VarT cur :
-                map TH.VarT rest )
-            tupTy' = foldl TH.AppT (TH.ConT (TH.tupleTypeName (n-1)))
-              (map TH.VarT rest)
-          return $
-            TH.TySynInstD ''TagsK (TH.TySynEqn [tupTy]
-              ( TH.PromotedConsT `TH.AppT` (TH.ConT ''KindOf `TH.AppT` TH.VarT tag) `TH.AppT`
-                (TH.ConT ''TagsK `TH.AppT` tupTy') ))
-        return tagsInstances
+    varName k = TH.mkName ('a' : show k)
+    tagName k = TH.mkName ('t' : show k)
+  for [2..tupCount] $ \n -> do
+    let
+      tupTy = List.foldl' TH.AppT (TH.ConT (TH.tupleTypeName n)) $
+        (\k -> TH.ConT ''Tagged `TH.AppT`
+          TH.VarT (tagName k) `TH.AppT`
+          TH.VarT (varName k)) <$> [0..n-1]
+      tagsList = List.foldr
+        (\a b -> TH.PromotedT 'HCons `TH.AppT` a `TH.AppT` b)
+        (TH.PromotedT 'HNil)
+        (TH.VarT . tagName <$> [0..n-1])
+    return $
+      TH.TySynInstD ''Tags (TH.TySynEqn [tupTy] tagsList)
+
 do
   let
     tupCount = 62
-    names    = [1..] >>= flip replicateM ['a'..'z']
-  varNames <- traverse TH.newName (take tupCount names)
+    varName k = TH.mkName ('a' : show k)
   fmap List.concat $
-    for (List.drop 2 (List.inits varNames)) $
-      \varNames' -> do
-        let n = List.length varNames'
-        tagsInstances <- for [() | n > 2] $ \() -> do
-          tag <- TH.newName "tag"
-          let
-            (cur:rest) = varNames'
-            tupTy = foldl TH.AppT (TH.ConT (TH.tupleTypeName n))
-              ( TH.ConT ''Tagged `TH.AppT` TH.VarT tag `TH.AppT` TH.VarT cur :
-                map TH.VarT rest )
-            tupTy' = foldl TH.AppT (TH.ConT (TH.tupleTypeName (n-1)))
-              (map TH.VarT rest)
-            tagsInst =
-              TH.TySynInstD ''Tags (TH.TySynEqn [tupTy]
-                ( TH.PromotedT 'HCons `TH.AppT` TH.VarT tag `TH.AppT`
-                  (TH.ConT ''Tags `TH.AppT` tupTy') ))
-          return tagsInst
-        hasLensInstances <- for [0..n-1] $ \k -> do
-          tag <- TH.newName "tag"
-          let
-            (prev, cur:next) = List.splitAt k varNames'
-            tupTy = foldl TH.AppT (TH.ConT (TH.tupleTypeName n))
-              ( map TH.VarT prev ++
-                [TH.ConT ''Tagged `TH.AppT` TH.VarT tag `TH.AppT` TH.VarT cur] ++
-                map TH.VarT next )
-          cur' <- TH.newName "x"
-          f <- TH.newName "f"
-          return $
-            TH.InstanceD Nothing []
-              (TH.ConT ''HasLens `TH.AppT` TH.VarT tag `TH.AppT` tupTy `TH.AppT` TH.VarT cur)
-              [ TH.FunD 'lensOf
-                  [ TH.Clause
-                      [TH.VarP f, TH.TupP
-                        ( map TH.VarP prev ++
-                          [TH.ConP 'Tagged [TH.VarP cur]] ++
-                          map TH.VarP next )]
-                      (TH.NormalB $
-                         TH.VarE 'fmap `TH.AppE`
-                           (TH.LamE [TH.VarP cur']
-                             (TH.TupE
-                              ( map TH.VarE prev ++
-                                [TH.ConE 'Tagged `TH.AppE` TH.VarE cur'] ++
-                                map TH.VarE next ))) `TH.AppE`
-                           (TH.VarE f `TH.AppE` TH.VarE cur) )
-                      [] ],
-                TH.PragmaD (TH.InlineP 'lensOf TH.Inline TH.FunLike TH.AllPhases) ]
-        return $ tagsInstances ++ hasLensInstances
+    for [2..tupCount] $ \n ->
+      for [0..n-1] $ \k -> do
+        let
+          tag = TH.mkName "tag"
+          prev = varName <$> [0..k-1]
+          cur  = varName k
+          next = varName <$> [k+1..n-1]
+          tupTy = foldl TH.AppT (TH.ConT (TH.tupleTypeName n))
+            ( map TH.VarT prev ++
+              [TH.ConT ''Tagged `TH.AppT` TH.VarT tag `TH.AppT` TH.VarT cur] ++
+              map TH.VarT next )
+        let
+          cur' = TH.mkName "x"
+          f = TH.mkName "f"
+        return $
+          TH.InstanceD Nothing []
+            (TH.ConT ''HasLens `TH.AppT` TH.VarT tag `TH.AppT` tupTy `TH.AppT` TH.VarT cur)
+            [ TH.FunD 'lensOf
+                [ TH.Clause
+                    [TH.VarP f, TH.TupP
+                      ( map TH.VarP prev ++
+                        [TH.ConP 'Tagged [TH.VarP cur]] ++
+                        map TH.VarP next )]
+                    (TH.NormalB $
+                       TH.VarE 'fmap `TH.AppE`
+                         (TH.LamE [TH.VarP cur']
+                           (TH.TupE
+                            ( map TH.VarE prev ++
+                              [TH.ConE 'Tagged `TH.AppE` TH.VarE cur'] ++
+                              map TH.VarE next ))) `TH.AppE`
+                         (TH.VarE f `TH.AppE` TH.VarE cur) )
+                    [] ],
+              TH.PragmaD (TH.InlineP 'lensOf TH.Inline TH.FunLike TH.AllPhases) ]
